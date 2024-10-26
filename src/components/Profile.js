8ac44fde869e80f54d10ae0ac818b6187d6a6261
Andy Gomez <45979588+Koato@users.noreply.github.com>
2024-10-26 05:49:24 +0200

    integracion back profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,37 +1,49 @@
 // src/components/Profile.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Profile() {
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Función para obtener los datos del perfil desde el servicio REST
+    const fetchProfileData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/sano/profile'); // URL del servicio REST
+        const data = await response.json();
+        setProfileData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error al obtener los datos del perfil:", error);
+        setLoading(true);
+      }
+    };
+
+    fetchProfileData();
+  }, []);
+
+  if (loading) return <p>Cargando datos del perfil...</p>;
+
   return (
     <div className="profile-page">
       <h2>Mi Perfil</h2>
       
       <div className="profile-section">
         <h3>Información del Usuario</h3>
-        <p><strong>Nombre:</strong> Juan Pérez</p>
-        <p><strong>Correo electrónico:</strong> juan.perez@example.com</p>
+        <p><strong>Nombre:</strong> {profileData.name}</p>
+        <p><strong>Correo electrónico:</strong> {profileData.email}</p>
         <button className="button edit-btn">Editar Perfil</button>
       </div>
 
       <div className="profile-section">
         <h3>Preferencias</h3>
         <ul>
-          <li>Evitar productos con gluten</li>
-          <li>Preferir productos orgánicos</li>
-          <li>Sin azúcar añadido</li>
+          {profileData.preferences.map((preference, index) => (
+            <li key={index}>{preference}</li>
+          ))}
         </ul>
         <button className="button edit-btn">Editar Preferencias</button>
       </div>
-
-      {/* <div className="profile-section">
-        <h3>Historial de Productos Escaneados</h3>
-        <ul>
-          <li>Producto 1 - Calificación: ★★★★☆</li>
-          <li>Producto 2 - Calificación: ★★★☆☆</li>
-          <li>Producto 3 - Calificación: ★★★★★</li>
-        </ul>
-        <button className="button clear-history-btn">Borrar Historial</button>
-      </div> */}
     </div>
   );
 }
